c1f602fb17a84b3b12f1ffde2d2b7a16e8078413
Marc Nijdam <marc_nijdam@mac.com>
2021-02-25 11:43:17 -0600

    Allow undefined down queries

--- src/psql_migration.erl
+++ src/psql_migration.erl
@@ -293,13 +293,18 @@ apply_migrations(Type, Migrations, Conn) ->
     lists:reverse(Results).
 
 apply_migration(Type, {Version, Migration}, Conn) ->
-    {ok, Query} = eql:get_query(Type, Migration),
-    case if_ok(?DRIVER:squery(Conn, Query)) of
-        ok ->
+    case eql:get_query(Type, Migration) of
+        {ok, Query} ->
+            case if_ok(?DRIVER:squery(Conn, Query)) of
+                ok ->
+                    record_migration(Type, Conn, Version),
+                    ok;
+                {error, Error} ->
+                    {error, Error}
+            end;
+        undefined ->
             record_migration(Type, Conn, Version),
-            ok;
-        {error, Error} ->
-            {error, Error}
+            ok
     end.
 
 if_ok(Rs) when is_list(Rs) ->

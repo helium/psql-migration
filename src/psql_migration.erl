11c8af749253ac57b3b16b971808ce316e6c3c67
vmalepati1 <malepativikas@gmail.com>
2021-06-30 00:14:57 -0400

    Use string:trim rather than regex replace

--- src/psql_migration.erl
+++ src/psql_migration.erl
@@ -193,7 +193,7 @@ connection_opts(Args, {env, URLName}) ->
         DatabaseUrl -> connection_opts(Args, {url, DatabaseUrl})
     end;
 connection_opts(_Args, {url, DatabaseUrl}) ->
-    case uri_string:parse(re:replace(DatabaseUrl, "\\r|\\n", "", [global,{return,list}])) of
+    case uri_string:parse(string:trim(DatabaseUrl)) of
         {error, Error, Term} ->
             {error, {Error, Term}};
         Map = #{userinfo := UserPass, host := Host, path := Path} ->
